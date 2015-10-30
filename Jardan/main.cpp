ffe9391788cf87c332e39e8c260781cb20ff52a9
vinkiril <vinkiril@mail.ru>
2015-10-30 21:28:11 +0400

    second

--- Jardan/main.cpp
+++ Jardan/main.cpp
@@ -1,8 +1,10 @@
 #include <QCoreApplication>
+#include <iostream>
+using namespace std;
 
-int main(int argc, char *argv[])
+int main()
 {
-    QCoreApplication a(argc, argv);
+    cout<<"hello world";
 
-    return a.exec();
+    return 0;
 }
