47a1cc4fa0e269484813ebf75c7b8d0072df195d
vinkiril <vinkiril@mail.ru>
2015-10-31 11:33:48 +0400

    need fix for b[i][j]

--- Jardan/main.cpp
+++ Jardan/main.cpp
@@ -1,42 +1,59 @@
 #include <QCoreApplication>
 #include <iostream>
 #include <vector>
+#include <iomanip>
 using namespace std;
 const int n=2;
 const int m=3;
-vector<double> input(int n,int m){
+vector<vector<double> > input(int n,int m){
 
-    vector<double>arr;
-    arr.resize(n*m);
+    vector<vector<double> >arr(n,vector<double>(m));
     cout<<"Enter array: "<<endl;
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
-            cin>>arr[i*n+j];
+            cin>>arr[i][j];
         }
     }
 
     return arr;
 }
 
-void output(vector <double> arr){
+void output(vector<vector <double> > arr){
     cout<<"Array: "<<endl;
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
-            cout<<arr[i*n+j]<<" ";
+            cout.precision(3);
+            cout<<setw(6)<<arr[i][j]<<" ";
         }
         cout<<endl;
     }
 }
 
-vector<double> Jardan(vector<double>arr){
-
-
-    return arr;
+vector<vector<double> > Jardan(vector<vector<double> >arr){
+    vector<vector<double> >jardArr(n,vector<double>(m));
+    cout<<"select element:[i][j] "<<endl;
+    int k,s;
+    cin>>k;
+    cin>>s;
+    for (int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if((i==k)&&(j==s))
+                jardArr[i][j]=1/arr[k][s];
+            if((i==k)&&(j!=s))
+                jardArr[i][j]=arr[i][j]/arr[k][s];
+            if((i!=k)&&(j==s))
+                jardArr[i][j]=arr[i][j]/arr[k][s]*(-1);
+            if((i!=k)&&(j!=s))//need fix
+                jardArr[i][j]=(arr[i][j]*arr[k][s]-arr[i][s]*arr[k][j])/arr[k][s];
+        }
+    }
+    return jardArr;
 }
 
 
 int main()
 {
-    output(input(n,m));
+
+    output(Jardan(input(n,m)));
     return 0;
 }
