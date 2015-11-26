7ad5e4b6edf1e69c4b4a1e49a2fd003772c69934
vinkiril <vinkiril@mail.ru>
2015-11-27 00:20:21 +0500

    автовыбор строки

--- Jardan/Jardan.pro
+++ Jardan/Jardan.pro
@@ -9,13 +9,10 @@ QT       += core
 QT       -= gui
 
 TARGET = Jardan
+CONFIG   += console
 CONFIG   -= app_bundle
 
 TEMPLATE = app
 
 
 SOURCES += main.cpp
-
-FORMS +=
-
-HEADERS +=
--- Jardan/main.cpp
+++ Jardan/main.cpp
@@ -6,7 +6,6 @@
 using namespace std;
 int n;
 int m;
-
 vector<vector<double> > input(){
     cout<<"Enter size matrix(x y):"<<endl;
     cin>>n;
@@ -39,43 +38,67 @@ vector<vector<double> > input(){
 void output(vector<vector <double> > arr){
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
-            cout.precision(3);
+            cout.precision(3);          //округление
             cout<<setw(6)<<arr[i][j]<<" ";
         }
         cout<<endl;
     }
 }
 
+int selectSTR(int STB,vector<vector<double> >arr){//выбираем строку при заднном столбце
+    int coord=0; //запомниает строку минимального частного
+    double div; //значение частного
+    double temp=1000;//предыдущее значение
+
+    for(int i=0;i<n;i++){
+          if(arr[i][STB]!=0){
+            div=arr[i][0]/arr[i][STB];
+            if(div<temp){
+                temp=div;
+                coord=i;
+            }
+        }
+    }
+
+    return coord;
+}
+
+
 vector<vector<double> > Jardan(vector<vector<double> >arr){
-    vector<vector<double> >jardArr(n,vector<double>(m));
-    cout<<"select element:[i][j] "<<endl;
+    vector<vector<double> >jardArr(n,vector<double>(m)); //отжарденный массив
+    cout<<"select column:  [j] (j>0)"<<endl;
     int k,s;
-    cin>>k;
-    cin>>s;
+    do{
+    cin>>s;                                                 //выбираем столбец
+
+    }while(s==0);
+    k=selectSTR(s,arr);                                 // cтрока выбирается автоматически
     cout<<"Selected element a["<<k<<"]["<<s<<"] = "<<arr[k][s]<<endl;
     for (int i=0;i<n;i++){
         for(int j=0;j<m;j++){
 
             if((i!=k)&&(j!=s))
-                jardArr[i][j]=(arr[i][j]*arr[k][s]-arr[i][s]*arr[k][j])/arr[k][s];
+                jardArr[i][j]=(arr[i][j]*arr[k][s]-arr[i][s]*arr[k][j])/arr[k][s];  //считаем прочие элемнты
             else
             {
                 if((i!=k)&&(j==s))
-                jardArr[i][j]=arr[i][j]/arr[k][s]*(-1);
+                jardArr[i][j]=arr[i][j]/arr[k][s]*(-1); // считаем выбраный столбец
                 if((i==k)&&(j!=s))
-                jardArr[i][j]=arr[i][j]/arr[k][s];
+                jardArr[i][j]=arr[i][j]/arr[k][s];      // считаем выбранную строку
                 if((i==k)&&(j==s))
-                jardArr[i][j]=1/arr[k][s];
+                jardArr[i][j]=1/arr[k][s];              //выбранный элемент делмаем обратным
             }
          }
     }
     return jardArr;
 }
 
+
 int main()
 {
 
-    vector<vector<double> > v=input();
+    vector<vector<double> > v;
+    v=input();
     cout<<"Entered array"<<endl;
     output(v);
     v=Jardan(v);
